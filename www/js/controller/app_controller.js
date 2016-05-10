189f1977f18c7c1f9952b1c1c03ca3d665417865
Safire Lauene <safire.lauene@gmail.com>
2016-05-10 11:43:53 -0300

    enviando arquivos

--- www/js/controller/app_controller.js
+++ www/js/controller/app_controller.js
@@ -1,6 +1,8 @@
+/* global angular */
 (function() {
 	"use strict";
     angular.module("myApp").controller("initCtrl", function($scope){
-       $scope.home = "Home" ;
+        $scope.home = "Home" ;
+        $scope.perfil = "Perfil";
     });
 })();
